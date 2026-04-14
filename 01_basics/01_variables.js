2cfc1665ea2487d3d1b2ec9f299df9393305346c
psy-dot <psy541484@gmail.com>
2026-04-14 07:37:14 +0000

    config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=1444553
+let accountEmail="akash@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+//accountId=2
+/*
+never use var because it is not professional
+ans also issue of block scope and functional scope
+*/
+accountEmail="asda@gmail"
+accountCity="delhi"
+
+console.log(accountId);
+
+console.table([accountEmail,accountCity])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"// treat all js code as newer version
+
+// code readability should be high
+
+let name="akash"
+let age=20
+let isLoggedIn= false
+
+//number=> 2 to power 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null => standalone value
+// undefined=>
+// symbol=> unique
+
+//object
+
+
+console.log(typeof null);
+
