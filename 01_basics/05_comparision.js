9eaf68d39845db0b0b9512476835ef5df3beaeed
psy-dot <psy541484@gmail.com>
2026-04-19 10:55:29 +0000

    config

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,46 @@
+//console.log("2">1);
+//console.log("02">1);
+
+//console.log(null>0);
+//console.log(null==0);
+//console.log(null>=0);
+
+//console.log(undefined==0);
+//console.log(undefined<0);
+//console.log(undefined>0);
+
+//console.log("2"===2);
+
+// primitive
+
+// 7 types: String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+const score= 100
+const scoreValue=100.3
+
+const isLoggedIn = false
+const outsideTemp= null
+let userEmail;
+const id= Symbol('123')
+const anotherId=Symbol('123')
+// Refrerce or Non primitive
+
+//console.log(id===anotherId);
+
+//const bigNumber= 13224332245n;
+//console.log(bigNumber)
+
+
+//Array,Objects,Functions
+
+const heros=["shaktiman","naagraj","doga"]
+let myObj={
+    name:"akash",
+    age:20,
+}
+
+const myFunction=function(){
+
+     console.log("Hello WOrld");
+}
+    
